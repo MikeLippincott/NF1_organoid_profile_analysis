6926df9948f2bf33c0692f69c31acc635ccd4819
MikeLippincott <1michaell2017@gmail.com>
2026-07-24 09:53:41 -0600

    udpate script to have overwrite setting

--- 0.download_data/scripts/setup_files.py
+++ 0.download_data/scripts/setup_files.py
@@ -4,7 +4,7 @@
 # This notebook unzips the data compressed file and sets up the data directory for analysis.
 # This way all users have the same data directory structure and files for analysis.
 
-# In[ ]:
+# In[1]:
 
 
 import pathlib
@@ -15,21 +15,21 @@ from notebook_init_utils import init_notebook
 root_dir, in_notebook = init_notebook()
 
 
-# In[7]:
+# In[2]:
 
 
-OVERWRITE = False
+OVERWRITE = True
 shippable_zip_path = pathlib.Path(f"{root_dir}/data/shippable_dir.zip").resolve(
     strict=True
 )
 
 
-# In[9]:
+# In[ ]:
 
 
 unpack_dir = pathlib.Path(f"{root_dir}/data/").resolve(strict=False)
 # check if unpack_dir is empty or not
-if unpack_dir.exists() and any(unpack_dir.iterdir()) and not OVERWRITE:
+if not OVERWRITE:
     print("Data directory is not empty and OVERWRITE is False. Skipping unpacking.")
 else:
     # unzip the shippable_dir.zip file to the data directory
