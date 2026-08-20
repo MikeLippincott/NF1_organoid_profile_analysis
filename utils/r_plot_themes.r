fb4cb94e5f04e4f94232c8cde532392141dee1ce
Mike Lippincott <58147848+MikeLippincott@users.noreply.github.com>
2026-08-19 18:07:40 -0600

    heatmaps for correlation across patients and treatments (#19)
    * heatmaps for correlation across patients and treatments
    
    * add each new script
    
    * addressing comments
    
    * addressing comments on color palette

--- utils/r_plot_themes.r
+++ utils/r_plot_themes.r
@@ -150,3 +150,9 @@ feature_type_palette = c(
     "Intensity" = brewer.pal(8, "Paired")[5],
     "Texture" = brewer.pal(8, "Paired")[8]
 )
+tab20_palette_for_patients <- c(
+    "#1f77b4", "#aec7e8", "#ff7f0e", "#ffbb78", "#2ca02c", "#98df8a",
+    "#d62728", "#ff9896", "#9467bd", "#c5b0d5", "#8c564b", "#c49c94",
+    "#e377c2", "#f7b6d2", "#7f7f7f", "#c7c7c7", "#bcbd22", "#dbdb8d",
+    "#17becf", "#9edae5"
+)
